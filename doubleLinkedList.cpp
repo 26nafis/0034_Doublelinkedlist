d86e3f68628b09ef357c5b863c3dbd35eddfed9c
26nafis <spalanzaninafis@gmail.com>
2025-05-15 10:35:12 +0700

    membuat doublelinkedlist

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -15,3 +15,15 @@ class DoubleLinkedList
 {
       private:
       node *START;
+
+      public:
+      DoubleLinkedList(){
+              START = NULL;
+      }
+      void addnode (){
+        int nim;
+        string nama;
+        cout << "\n Enter the roll nimber of the student";
+        cin >> nim;
+      }
+};
