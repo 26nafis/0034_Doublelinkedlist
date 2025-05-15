1cce4fcd6cb69a33eb600f0d78970dca338e4c98
26nafis <spalanzaninafis@gmail.com>
2025-05-15 10:41:51 +0700

    pentisipan node kedalam double linked list

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -30,3 +30,7 @@ class DoubleLinkedList
         // step 2 assign value to the data fields
         newNode -> noMhs = nim;
 
+        // step 3 insret at begining if list is empty or nim is smalled
+        if (START == NULL || nim)
+      }
+};
